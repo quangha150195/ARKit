98b4327ddc67da0639b969598d76b929fcd2a400
habq <habq@onetech.vn>
2017-07-19 17:57:22 +0700

    + ScrollView Objects

--- ARKit/Assets/_Scripts/MainScene.cs
+++ ARKit/Assets/_Scripts/MainScene.cs
@@ -2,12 +2,17 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MainScene : MonoBehaviour {
+    [SerializeField]
+    private GameObject m_ScrollViewObject;
+    [SerializeField]
+    private Toggle m_BtnHiddemScrollView;
 
     // Use this for initialization
     void Start () {
-		
+       
 	}
 	
 	// Update is called once per frame
@@ -15,6 +20,24 @@ public class MainScene : MonoBehaviour {
 		
 	}
 
+    public void addObject(string _name)
+    {
+        GameObject gameObj = Instantiate(Resources.Load("_Model/" + _name),new Vector3(0,0,100), Quaternion.identity) as GameObject;
+    }
+
+    public void showScrollView()
+    {
+        if(m_BtnHiddemScrollView.isOn)
+        {
+            iTween.MoveTo(m_ScrollViewObject, iTween.Hash("x", m_ScrollViewObject.transform.position.x + 250, "time", 1.0f));
+            iTween.RotateTo(m_BtnHiddemScrollView.GetComponentInChildren<Image>().gameObject, iTween.Hash("y", 180, "time", 2.0f));
+        }
+        else
+        {
+            iTween.MoveTo(m_ScrollViewObject, iTween.Hash("x", 1222.5f, "time", 1.0f));
+            iTween.RotateTo(m_BtnHiddemScrollView.GetComponentInChildren<Image>().gameObject, iTween.Hash("y", 0, "time", 2.0f));           
+        }
+    }
 
 
 }
